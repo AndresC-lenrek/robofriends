03089989f52846edf6df19847f92ce3a55712120
andres <53791842+AnrdesCoronado@users.noreply.github.com>
2020-03-25 21:37:25 -0600

    update

---
+++ precache-manifest.f94feeb8a57abb5b94ffd37422bc3eab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20b4f8e9520413b148d72513ee289c7f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "885a4f9505d9391bb37b",
+    "url": "/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "7aa63acc8c84ebde86e0",
+    "url": "/static/css/main.2fbec3d8.chunk.css"
+  },
+  {
+    "revision": "885a4f9505d9391bb37b",
+    "url": "/static/js/2.35164f70.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.35164f70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7aa63acc8c84ebde86e0",
+    "url": "/static/js/main.bdaa8f98.chunk.js"
+  },
+  {
+    "revision": "3a172e9daf29f6cc3071",
+    "url": "/static/js/runtime-main.08f98dcf.js"
+  }
+]);
