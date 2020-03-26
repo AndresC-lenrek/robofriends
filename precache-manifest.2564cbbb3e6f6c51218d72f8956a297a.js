9183c08fe194e0095f953d4dbab8900165bdb3f8
andres <53791842+AnrdesCoronado@users.noreply.github.com>
2020-03-25 22:22:22 -0600

    Updates

---
+++ precache-manifest.2564cbbb3e6f6c51218d72f8956a297a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84f9fe894149970d167b3117986b7249",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "0c424d4dd9fa80c3b2fe",
+    "url": "/robofriends/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "e2585feb16346a0c2e12",
+    "url": "/robofriends/static/css/main.2fbec3d8.chunk.css"
+  },
+  {
+    "revision": "0c424d4dd9fa80c3b2fe",
+    "url": "/robofriends/static/js/2.0761117a.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/robofriends/static/js/2.0761117a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2585feb16346a0c2e12",
+    "url": "/robofriends/static/js/main.a72a75cd.chunk.js"
+  },
+  {
+    "revision": "46690a7b9ddc777b3b4c",
+    "url": "/robofriends/static/js/runtime-main.ba4515c0.js"
+  }
+]);
